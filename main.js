511ca1a473e08335bc2ce5f37b72045ce2d604d5
celebrater <tkgm45nbchk@gmail.com>
2022-02-18 19:20:53 +0900

    行末文字の取得に成功

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const box = document.getElementById("box");
+const children = box.children;
+const fontSize = 20;
+const maxChars = Math.floor(children[1].clientWidth / fontSize);
+
+const separateLine = () => {
+    console.log(children);
+    // let i = 1;
+    // children.map((child) => {
+    //     console.log(child.innerHTML.substr(maxChars, 1));
+    // });
+    for(let i = 1; i < children.length; i++){
+        console.log(children[i].innerHTML.substr(maxChars - 1, 1));
+    }
+}
